391f9009284419045725034c3251b6cb72dba0ee
Szász István Zoltán <istvanszasz99@gmail.com>
2025-05-28 13:15:09 +0200

    Profile, DetailsPage Implementation + DetailsPage backend Implementation

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import SearchPage from './components/SearchPage/SearchPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
+import Profile from './components/Profile/Profile';
 
 function App() {
   const navigate = useNavigate();
@@ -15,10 +17,12 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage />} />
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/profile" element={<Profile />} />
         </Routes>
         </>
   );
